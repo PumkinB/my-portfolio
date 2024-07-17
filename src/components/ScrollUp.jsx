fa20f3c1983bcd9b2c2df538e16f9ec8d0839c9a
PumkinB <olipumknb@gmail.com>
2024-07-17 13:32:58 +0700

    fix scroll up

--- src/components/ScrollUp.jsx
+++ src/components/ScrollUp.jsx
@@ -14,7 +14,7 @@ const ScrollUp = () => {
   return (
     <a
       href="#home"
-      className={`fixed right-10 rounded-full bg-purple-700 py-2 px-2 text-white flex justify-center text-4xl hover:opacity-80 animate-bounce ${
+      className={`fixed right-6 md:right-10 rounded-full bg-purple-700 py-2 px-2 text-white flex justify-center text-4xl hover:opacity-80 animate-bounce ${
         isVisible ? "bottom-[75px] md:bottom-10" : "bottom[-20%]"
       }`}
     >
